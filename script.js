fd1a188d9cd978e1b7a8963966e31c7fd9e86ca9
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-06-01 16:04:22 +0100

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById('modal');
+const modelShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmarfk-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// show modal. focus on input
